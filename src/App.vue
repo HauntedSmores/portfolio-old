6dea83f5af64b67e36743e2afb3ed88d7396fae9
Darren Segal <me@darren-segal.com>
2018-05-02 17:52:20 -0400

    Refactored

--- src/App.vue
+++ src/App.vue
@@ -1,179 +1,29 @@
 <template>
   <div id="app">
-	  <canvas id="snow"></canvas>
-      <global-nav v-bind:active="text_animation"></global-nav>
-      <div id="intro_animation" v-show="landing">
-          <div id="text_panel">
-              <span class="letter" v-for="letter in welcome_message">{{ letter }}</span>
-              <!-- <span class="letter">e</span>
-              <span class="letter">l</span>
-              <span class="letter">c</span>
-              <span class="letter">o</span>
-              <span class="letter">m</span>
-              <span class="letter">e</span> -->
-          </div>
-          <div id="intro_line"></div>
-      </div>
-    <router-view></router-view>
+    <Canvas/>
   </div>
 </template>
 
 <script>
-import anime from 'animejs';
-import * as pixi from 'pixi.js';
-import GlobalNav from './components/global_nav';
-import Home from './components/home';
+import Canvas from "@/components/Canvas";
 
 export default {
-	name: 'app',
-	components: {
-	  GlobalNav,
-	  Home
-	},
-	data() {
-	  return {
-	      text_animation: true,
-		  landing: false,
-		  welcome_message: 'Welcome'
-	  }
-	},
-	mounted() {
-
-		this.welcome_message = this.welcome_message.split('');
-
-		console.log(this.$route.path == '/');
-		if (this.$route.name) {
-			this.landing = false;
-			this.text_animation = false;
-		} else {
-			this.landing = true;
-			anime({
-				targets: '#intro_line',
-				left: '50%',
-				width: '100%',
-				duration: 2000,
-				easing: 'easeInOutCubic',
-				complete: () => {
-					anime({
-						targets: '#intro_line',
-						width: '0',
-						left: ['50%', '0%'],
-						duration: 1200,
-						delay: 2000,
-						easing: 'easeInOutCubic',
-						complete: () => {
-							this.text_animation = false;
-							this.$router.push('home');
-						}
-					});
-				}
-			});
-
-			anime({
-				targets: '.letter',
-				translateY: ['100%', '0%'],
-				opacity: 1,
-				duration(el, i) {
-				  return 1000 + (Math.pow(i, 2) * 10);
-				},
-				delay(el, i) {
-				  return 800 + (i * 100);
-				},
-				elasticity: 100,
-				direction: 'alternate'
-			});
-		}
-
-		let canvas = document.getElementById('snow');
-		let renderer = pixi.autoDetectRenderer(document.documentElement.clientWidth, document.documentElement.clientHeight, {view: canvas, transparent: true, antialias: true});
-		renderer.resize = true;
-		let stage = new pixi.Container();
-
-		this.snow_machine(stage, renderer);
-
-		setInterval(() => { this.snow_machine(stage, renderer) }, 15000);
-
-
-	},
-	methods: {
-		snow_machine(stage, renderer) {
-			let snow = [];
-			for (let i = 0; i < 10; i++) {
-				let circle = new pixi.Graphics();
-				let x = Math.floor((Math.random() * window.innerWidth) + 10);
-				circle.beginFill('0xFCFFFD');
-				circle.drawCircle(x, window.innerHeight, 2);
-				circle.endFill();
-				circle.filters = [new PIXI.filters.BlurFilter(1)];
-				stage.addChild(circle);
-				snow.push(circle);
-			}
-			anime({
-				targets: snow,
-				y: -(window.innerHeight + 4),
-				duration: 8000,
-				easing: 'linear',
-				delay: (el, i) => {
-					return i * 1000
-				},
-				update: () => {
-					renderer.render(stage);
-				},
-				complete: () => {
-					for (let circle of snow) {
-						circle.destroy(true);
-					}
-				}
-			});
-		}
-	}
-}
+  name: "App",
+  components: {
+    Canvas
+  }
+};
 </script>
 
 <style lang="scss">
-    @import './assets/scss/main';
+    @import '~@/assets/scss/main';
 
     #app {
-		position: relative;
-        background-color: $blue;
-        height: 100%;
-		background: linear-gradient(45deg, rgba(250,200,205,1) 5%,rgba(191,215,234,1) 50%,rgba(191,215,234,1) 50%,rgba(128,255,232,1) 100%);
+      font-family: "Avenir", Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      color: #2c3e50;
+      background-color: #201d35;
     }
-
-	#snow {
-		position: fixed;
-		top: 0;
-		left: 0;
-	}
-
-    #intro_animation {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-    }
-
-    #text_panel {
-        white-space: nowrap;
-        overflow: hidden;
-    }
-
-    #intro_line {
-        position: absolute;
-        left: 0;
-        transform: translateX(-50%);
-        height: 1px;
-        background-color: $white;
-    }
-
-    .letter {
-        @extend h1;
-        font-size: 3rem;
-        opacity: 0;
-        display: inline-block;
-        margin: 0;
-        line-height: 1;
-		color: $white;
-    }
-
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '../components/home'
 
 Vue.use(Router)
 
 export default new Router({
-	routes: [
-		{
-			path: '/home',
-			name: 'home',
-			component: Home
-		},
-		{
-			path: '/work',
-			name: 'work'
-		},
-		{
-			path: '/contact',
-			name: 'contact',
-		}
-	]
+	routes: []
 })
